f60c25dbd41cfcbe61460ede4bcd4ed434cc731c
nuke7 <gombos.marton@icloud.com>
2021-02-25 18:51:22 +0100

    e.preventDefault()

--- frontend/public/script.js
+++ frontend/public/script.js
@@ -1,4 +1,8 @@
 function _loaded() {
   console.log("The client is running...");
+  document.querySelector("input[type='submit']").addEventListener("click", (e) => {
+    e.preventDefault();
+    console.log("We blocked the file upload");
+  });
 }
 window.addEventListener("load", _loaded);
